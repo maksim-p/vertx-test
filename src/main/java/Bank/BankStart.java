874a6da300f30946d7c37d3b3cf5d90916d1f559
makism-p <maksim.petrikov9@gmail.com>
2018-12-10 18:07:08 +0300

    rename Bank

--- src/main/java/Bank/BankStart.java
+++ src/main/java/Bank/BankStart.java
@@ -3,14 +3,14 @@ package Bank;
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Future;
 
-public class Bank extends AbstractVerticle {
+public class BankStart extends AbstractVerticle {
 
   @Override
   public void start(Future<Void> startFuture) throws Exception {
     vertx.createHttpServer().requestHandler(req -> {
       req.response()
         .putHeader("content-type", "text/plain")
-        .end("Hello from Bank");
+        .end("Hello from BankStart");
     }).listen(8081, http -> {
       if (http.succeeded()) {
         startFuture.complete();
